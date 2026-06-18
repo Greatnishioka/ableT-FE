68b31eab9c9bec062ffb3a975df09ec226c6f0f2
Greatenishioka <20e1118@andrew.ac.jp>
2026-06-18 15:17:04 +0900

    コメントを追記

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -5,6 +5,7 @@ import type { paths } from "@/src/shared/api/schema";
 
 // 腐敗防止層の入り口です。もし、APIクライアントの実装を差し替える必要が出てきた場合は、
 // このファイルもしくはinfrastructureのファイルのみを編集すれば問題ないようにする。
+// できれば、infrastructureのファイルも編集せずに差し替えられるように徹底してください。
 export const Fetcher = createClient<paths>({
   baseUrl: apiBaseUrl,
   credentials: "include",
